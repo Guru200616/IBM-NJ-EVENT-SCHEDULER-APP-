23660944d6ac9df3de0f61d55ce2b07cea55f313
Guru200616 <168887288+Guru200616@users.noreply.github.com>
2025-10-31 20:50:03 +0530

    Delete Event.kt
    Signed-off-by: Guru200616 <168887288+Guru200616@users.noreply.github.com>

--- Event.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eventscheduler
-
-data class Event(
-    val id: String,
-    val title: String,
-    val description: String,
-    val date: String,
-    val location: String
-)
